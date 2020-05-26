dfcc18337498b6b6f17645c66047a50f7addba13
Milly Flores <miflo0h@gmail.com>
2020-05-26 19:09:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,55 @@
 # Make your shoe class here!
+class Shoe 
+  
+  def initialize(shoe)
+    @shoe = shoe 
+  end
+  
+  def shoe
+    @shoe
+  end 
+  
+  def brand=(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color 
+    @color 
+  end
+  
+  def size=(size)
+    @size = size
+  end 
+  
+  def size 
+    @size 
+  end 
+  
+  def material=(material)
+    @material = material
+  end 
+  
+  def material
+    @material 
+  end
+  
+  def condition=(condition)
+      @condition = condition
+  end 
+  def condition
+    @conditition
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition= "new"
+  end
+end 
